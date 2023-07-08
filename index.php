d22e2c3a454ede18200462ae72b6bf7e1542123a
Hadi <asusn43@yahoo.com>
2023-07-08 18:00:25 +0330

    init

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+
+
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = $conn->query($sql);
 
