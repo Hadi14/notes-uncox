a65e5c1a28964721e3e275564ca446ec592a40ff
Hadi <asusn43@yahoo.com>
2023-07-09 17:16:58 +0330

    ok

---
+++ db.php
@@ -0,0 +1,65 @@
+<?
+class Db
+{
+    private $con;
+    public function __construct($options = null)
+    {
+        if ($options) {
+            $servername = $options['host'];
+            $username = $options['user'];
+            $password = $options['pass'];
+            $dbname = $options['db'];
+        } else {
+            global $config;
+            $servername = $config['db']['host'];
+            $username = $config['db']['user'];
+            $password = $config['db']['pass'];
+            $dbname = $config['db']['dbname'];
+        }
+        $this->con = new mysqli($servername, $username, $password, $dbname);
+        if ($this->con->connect_error) {
+            echo "Connection failed: " . $this->con->connect_error;
+            exit;
+        }
+        // echo "test <br>";
+        // $this->con->query("SET NAMES 'utf8'");
+    }
+    public function connection($sql)
+    {
+        return  $this->con;
+    }
+
+    public function first($sql)
+    {
+        $result = $this->con->query($sql);
+        if ($result == null) {
+            return null;
+        }
+        return $result[0];
+    }
+    public function doquery($sql)
+    {
+        $result = $this->con->query($sql);
+        $records = array();
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $records[] = $row;
+            }
+            // echo "<pre>" . print_r($result) . "<pre>";
+            var_dump($result);
+            echo "<br>";
+            print_r($result);
+            echo "<br>";
+            $out = print_r($records, true);
+            echo "<pre>$out <pre>";
+            // return $records;
+        } else {
+
+            return null;
+        }
+    }
+    public function close()
+    {
+        $this->con->close();
+    }
+}
--- index.php
+++ index.php
@@ -1,26 +1,8 @@
-<? $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "note";
+<?
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require_once("db.php");
+require_once("config.php");
+$db = new Db();
 
-
-
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
+$sql = "SELECT * FROM x_note";
+$result = $db->doquery($sql);
