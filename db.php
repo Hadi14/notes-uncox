27551f0de009e6636e187d78064a3045e62751a3
Hadi <asusn43@yahoo.com>
2023-07-10 15:36:06 +0330

    ok

--- db.php
+++ db.php
@@ -51,6 +51,7 @@ class Db
 
             return null;
         }
+        return $records;
     }
     /*************************************************************************** */
     public function close()
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 require_once("db.php");
 require_once("config.php");
+require_once("common.php");
 $db = new Db();
 
 $sql = "SELECT * FROM x_note";
 $result = $db->doquery($sql);
+dump($result);
