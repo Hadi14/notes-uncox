6a0c1c0e70d2de3bd86b93c745d501a587867c3e
Hadi <asusn43@yahoo.com>
2023-07-14 21:10:08 +0330

    correct md5

--- login-check.php
+++ login-check.php
@@ -1,16 +1,23 @@
 <?
 
+// echo md5("123");
+// $m = md5("123");
+// if (md5("123") == "202cb962ac59075b964b07152d234b70") {
+//     echo " Matched";
+// }
+// exit;
+
+
+
 require_once("main.php");
 
 $db = Db::getInstance();
 $user = $_POST['user'];
 $pass = $_POST['pass'];
-$records = $db->doquery("select  * from x_users where username='$user' and pass='$pass'");
+$hashedpass = md5($pass);
+$records = $db->doquery("select  * from x_users where username='$user' and pass='$hashedpass'");
 if (!$records) {
 
-    if ('123' == md5('123')) {
-        echo "<br> Matched";
-    }
     $message = "login Failed";
     require_once("fail.php");
     exit;
